2320ebb27c8416e246c63d46f95d8ecd098c338c
sachiniwathudura <sachiniwathudura@gmail.com>
2025-02-13 22:39:46 +0530

    setup project

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+
+import {Text, View} from "react-native";
+import {Link} from "expo-router";
+
+function Index(){
+    return(
+        <View>
+            <Text>Dashboard</Text>
+        </View>
+    );
+}
+
+export default Index;
